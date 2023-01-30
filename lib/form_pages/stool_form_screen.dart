85780e6adf07073c02612ea65ce29bf8c47adcfe
grcjariten <giusepperussococo@gmail.com>
2023-01-30 19:28:29 +0100

    Created stool_form_screen.dart using mood_form_screen.dart as a model.
    Now working on save_buttons.dart

--- lib/form_pages/stool_form_screen.dart
+++ lib/form_pages/stool_form_screen.dart
@@ -1,17 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:calendario/utils/settings.dart';
+import 'package:intl/intl.dart';
+import '../form_ui/value_pickers.dart';
 
-class StoolFormPage extends StatelessWidget {
+
+class StoolFormPage extends StatefulWidget {
   const StoolFormPage({Key? key, required this.dateSelected}) : super(key: key);
   final DateTime dateSelected;
 
+  @override
+  State<StoolFormPage> createState() => _StoolFormPageState();
+}
+
+class _StoolFormPageState extends State<StoolFormPage> {
+  late DateTime dateSelected = widget.dateSelected;
+  double stoolSelected = 7.0;
+
+  newDateSelectedCallBack(DateTime daySelected) {
+    setState(() {
+      dateSelected = DateTime.parse("${DateFormat("yyyy-MM-dd HH:mm:ss").format(daySelected)}""Z");
+    });
+  }
+
+  _sliderCallBack(double newValue) {
+    setState(() {
+      stoolSelected = newValue;
+    });
+  }
+
+  Form stoolForm(BuildContext context, DateTime dateSelected,
+      dayPickedCallBack) {
+    GlobalKey<FormState> stoolFormKey = GlobalKey<FormState>();
+
+
+    return Form(
+      key: stoolFormKey,
+      child: Column(
+        children: [
+          Text(stoolSelected.toInt().toString()),
+          moodStoolValuePicker(stoolSelected, _sliderCallBack,
+              stoolPageAppBarColor),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: datePicker(context, dateSelected,
+                  dayPickedCallBack, stoolPageAppBarColor)),
+          // saveButton(context, moodFormKey, moodSelected, dateSelected)
+          // TODO: Work on saveButton first.
+        ],
+
+      ),
+    );
+
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: stoolPageAppBarColor,
         title: const Text("Insert a new stool!"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: stoolForm(context, dateSelected, newDateSelectedCallBack),
       ),
     );
   }
